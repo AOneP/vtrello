22318186262824b94117d5097a22ed036969fb56
AOneP <adrian1314@interia.pl>
2020-01-30 13:26:14 +0100

    List CRUD

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,55 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    list
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    list = List.create(list_params)
+    if list.save
+      redirect_to boards_path, notice: 'Stworzyłeś nową listę'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    list
+  end
+
+  def update
+    list
+    if list.update(list_params)
+      redirect_to boards_path, notice: 'Lista aktualizowana'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if list.destroy
+      redirect_to lists_path, notice: 'Usunąłeś listę'
+    else
+      render :index
+    end 
+  end
+
+  private
+
+  def list
+    @list ||= List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:title, :done)
+  end
+
+end
