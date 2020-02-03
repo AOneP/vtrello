a0f28d823327d4e2a1e961d0c885b38643168d3f
AOneP <adrian1314@interia.pl>
2020-02-03 12:29:53 +0100

    Hide current language in partial locales

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -3,7 +3,7 @@ class LocalesController < ApplicationController
 
   def show
     cookies['locale'] = params[:locale]
-    redirect_to root_path, notice: I18n.t('locales.show')
+    redirect_to root_path, notice: I18n.t('locales.show', locale: params[:locale].to_sym, new_language: params[:locale].upcase)
   end
 
 end
