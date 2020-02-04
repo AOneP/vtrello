820d208a2d089bb4e89d22f7a2c6f2a9ca719cae
AOneP <adrian1314@interia.pl>
2020-02-04 11:49:43 +0100

    Boards view and controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,6 +9,7 @@ class ListsController < ApplicationController
   end
 
 <<<<<<< HEAD
+<<<<<<< HEAD
   def create
     board
     list = board.lists.new(list_params)
@@ -19,17 +20,29 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+=======
+>>>>>>> 53c8c4b... Syntax fix
   def create
-    list = List.create(list_params)
+    board
+    list = board.lists.new(list_params)
     if list.save
+<<<<<<< HEAD
+<<<<<<< HEAD
       redirect_to boards_path, notice: 'Stworzyłeś nową listę'
 >>>>>>> 2231818... List CRUD
+=======
+      redirect_to boards_path, notice: I18n.t('lists.notifications.create')
+>>>>>>> 72a612f... Add List locales
+=======
+      redirect_to board_path(board), notice: I18n.t('lists.notifications.create')
+>>>>>>> 53c8c4b... Syntax fix
     else
       render :new
     end
   end
 
   def edit
+<<<<<<< HEAD
 <<<<<<< HEAD
     board
     @list = board.lists.find(params[:id])
@@ -41,19 +54,32 @@ class ListsController < ApplicationController
       redirect_to board_path(board), notice: I18n.t('lists.notifications.update')
 =======
     list
+=======
+    board
+    @list = board.lists.find(params[:id])
+>>>>>>> 53c8c4b... Syntax fix
   end
 
   def update
-    list
+    board
     if list.update(list_params)
+<<<<<<< HEAD
+<<<<<<< HEAD
       redirect_to boards_path, notice: 'Lista aktualizowana'
 >>>>>>> 2231818... List CRUD
+=======
+      redirect_to boards_path, notice: I18n.t('lists.notifications.update')
+>>>>>>> 72a612f... Add List locales
+=======
+      redirect_to board_path(board), notice: I18n.t('lists.notifications.update')
+>>>>>>> 53c8c4b... Syntax fix
     else
       render :edit
     end
   end
 
   def destroy
+<<<<<<< HEAD
 <<<<<<< HEAD
     board
     if list.destroy
@@ -62,12 +88,19 @@ class ListsController < ApplicationController
       render :index
     end
 =======
+=======
+    board
+>>>>>>> 53c8c4b... Syntax fix
     if list.destroy
-      redirect_to lists_path, notice: 'Usunąłeś listę'
+      redirect_to board_path(board), notice: I18n.t('lists.notifications.destroy')
     else
       render :index
+<<<<<<< HEAD
     end 
 >>>>>>> 2231818... List CRUD
+=======
+    end
+>>>>>>> 72a612f... Add List locales
   end
 
   private
@@ -77,12 +110,18 @@ class ListsController < ApplicationController
   end
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 53c8c4b... Syntax fix
   def board
     @board ||= Board.find(params[:board_id])
   end
 
+<<<<<<< HEAD
 =======
 >>>>>>> 2231818... List CRUD
+=======
+>>>>>>> 53c8c4b... Syntax fix
   def list_params
     params.require(:list).permit(:title, :done)
   end
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,5 @@
 class Board < ApplicationRecord
 
   has_many :lists
-  
+
 end
